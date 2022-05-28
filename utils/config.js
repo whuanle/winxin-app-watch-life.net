7a4826269c8255753be21bde3ab7f1f6d0eebc73
whuanle <1586052146@qq.com>
2022-05-28 11:50:32 +0800

    对接

--- utils/config.js
+++ utils/config.js
@@ -13,14 +13,14 @@
 
 //配置域名,域名只修改此处。
 //如果wordpress没有安装在网站根目录请加上目录路径,例如："www.watch-life.net/blog"
-var DOMAIN = "www.watch-life.net";
-var WEBSITENAME="守望轩"; //网站名称
-var PAGECOUNT='10'; //每页文章数目
-var WECHAT='微信号：iamxjb'; //客服联系方式,如 微信号：iamxjb 或 邮箱：iamxjb@sina.com
+var DOMAIN = "www.whuanle.cn";
+var WEBSITENAME="面向云技术架构 痴者工良"; //网站名称
+var PAGECOUNT='20'; //每页文章数目
+var WECHAT='微信号：whuanlex'; //客服联系方式,如 微信号：iamxjb 或 邮箱：iamxjb@sina.com
 
 //是否启用小程序扫描二维码登录网站,  true 启用  false  不启用
 //未安装微慕登录插件不要启用,插件下载地址：https://shops.minapper.com/2167.html
-const enableScanLogin =false 
+const enableScanLogin =true 
 //////////////////////////////////////////////////////
 
 //是否启用微慕视频号插件,  true 启用  false  不启用
